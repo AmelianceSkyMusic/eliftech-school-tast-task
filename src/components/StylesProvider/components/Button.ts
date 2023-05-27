028f3abadc3d9114c3d4976ac81ea5d214d0fe2d
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-05-27 12:45:57 +0300

    ^ fix Button style

--- src/components/StylesProvider/components/Button.ts
+++ src/components/StylesProvider/components/Button.ts
@@ -1,29 +1,8 @@
-import type { MantineTheme } from '@mantine/core';
-
 export const Button = {
 	Button: {
-		styles: (theme: MantineTheme) => ({
+		styles: () => ({
 			root: {
 				transition: 'background-color 0.2s linear, box-shadow 0.2s linear',
-				'&:hover': {
-					boxShadow: `0px 0px 8px 4px ${
-						theme.colorScheme === 'dark'
-							? theme.fn.rgba(
-								theme.colors[theme.primaryColor][theme.fn.primaryShade() - 2],
-								0.5,
-							)
-							: theme.fn.rgba(
-								theme.colors[theme.primaryColor][theme.fn.primaryShade() - 2],
-								0.5,
-							)
-					}`,
-					backgroundColor: theme.colors[theme.primaryColor][theme.fn.primaryShade() - 1],
-				},
-				'&:active': {
-					transition: 'background-color 0s linear, box-shadow 0s linear',
-					backgroundColor: theme.colors[theme.primaryColor][theme.fn.primaryShade() + 1],
-					boxShadow: `0px 0px 4px 4px ${theme.fn.rgba(theme.colors[theme.primaryColor][theme.fn.primaryShade()], 0.2)}`,
-				},
 			},
 		}),
 	},
