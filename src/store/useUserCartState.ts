7b93f4696c4777cef3ef01b8591b4729a877555d
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-05-28 01:32:57 +0300

    * add emptyCart/increment/decrement functions to useUserCartState

--- src/store/useUserCartState.ts
+++ src/store/useUserCartState.ts
@@ -1,17 +1,14 @@
 import { create } from 'zustand';
+import { CartItem } from '~types/Cart';
 import { ProductItem } from '~types/Product';
-import { ShopItem } from '~types/Shop';
-
-interface CartItem {
-	shopId: ShopItem['id'];
-	product: ProductItem;
-	count: number;
-}
 
 interface UseUserCartState {
 	userCart: CartItem[];
 	addProductToCart: ({ shopId, product, count }: CartItem) => void;
+	emptyCart: () => void;
 	removeProductFromCart: (productId: ProductItem['id']) => void;
+	incrementProductInCart: (productId: ProductItem['id']) => void;
+	decrementProductInCart: (productId: ProductItem['id']) => void;
 }
 
 export const useUserCartState = create<UseUserCartState>((set) => ({
@@ -20,11 +17,37 @@ export const useUserCartState = create<UseUserCartState>((set) => ({
 		set((state) => {
 			return { userCart: [...state.userCart, { shopId, product, count }] };
 		}),
+	emptyCart: () => set({ userCart: [] }),
 	removeProductFromCart: (productId) =>
 		set((state) => {
 			const filteredUserCart = state.userCart.filter(
 				(cartItem) => cartItem.product.id !== productId
 			);
 			return { userCart: filteredUserCart };
+		}),
+	incrementProductInCart: (productId) =>
+		set((state) => {
+			const newUserCart = state.userCart.map((cartItem) => {
+				if (cartItem.product.id === productId) {
+					const newCount = cartItem.count + 1;
+					return {
+						...cartItem,
+						count: newCount >= 1000 ? 1000 : newCount,
+					};
+				}
+				return cartItem;
+			});
+			return { userCart: newUserCart };
+		}),
+	decrementProductInCart: (productId) =>
+		set((state) => {
+			const newUserCart = state.userCart.map((cartItem) => {
+				if (cartItem.product.id === productId) {
+					const newCount = cartItem.count - 1;
+					return { ...cartItem, count: newCount <= 0 ? 0 : newCount };
+				}
+				return cartItem;
+			});
+			return { userCart: newUserCart };
 		}),
 }));
